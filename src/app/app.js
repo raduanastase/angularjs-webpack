cdd206f302779c2b3be661e49332e4124b98b294
raduanastase <raduanastasedev@gmail.com>
2017-03-01 23:12:23 +0200

    Adding first logic steps for word game

--- src/app/app.js
+++ src/app/app.js
@@ -6,13 +6,42 @@ let app = () => {
   return {
     template: require('./app.html'),
     controller: 'AppCtrl',
-    controllerAs: 'app'
+    controllerAs: 'appCtrl'
   }
 };
 
 class AppCtrl {
   constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
+    this.url = 'test';
+    this.wordList = this.shuffle(['PIZZA', 'TOMORROW', 'MONITOR', 'COMPUTER']);
+    this.wordIncrement = 0;
+    this.randomScrambledWord = this.getRandomScrambledWord();
+    this.randomSolvedWord = this.getRandomSolvedWord();
+    this.maxScore = this.calculateScore(this.randomScrambledWord, 0);
+  }
+
+  shuffle(array) {
+    for (let i = array.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [array[i - 1], array[j]] = [array[j], array[i - 1]];
+    }
+
+    return array;
+  }
+
+  getRandomSolvedWord() {
+    return this.wordList[this.wordIncrement];
+  }
+
+  getRandomScrambledWord() {
+    return this.shuffle(this.getRandomSolvedWord().split('')).join('')
+  }
+
+  calculateScore(word, mistakesCount) {
+    const maxScore = Math.floor(Math.pow(1.95, parseInt(word.length / 3)));
+    const finalScore = maxScore - mistakesCount;
+
+    return finalScore < 0 ? 0 : finalScore;
   }
 }
 
